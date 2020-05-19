bc115b2f1d43ab4c979b5a8947b319bedfa48d9b
Niks Veipans <nikins113@gmail.com>
2020-05-19 18:17:04 +0300

    correction not working correctly anymore

--- app/src/main/java/com/example/nils/botaniskietermini/Spelling.java
+++ app/src/main/java/com/example/nils/botaniskietermini/Spelling.java
@@ -1,19 +1,21 @@
 package com.example.nils.botaniskietermini;
 
+import android.util.Log;
+
 import java.io.*;
 import java.util.*;
 
 class Spelling {
 
-    private final Map<String, Double> dictionary = new HashMap<String, Double>();
+    private final Map<String, Double> dictionary = new HashMap<>();
 
-    List<Character> charList = new ArrayList<Character>();
+    List<Character> charList = new ArrayList<>();
     String charString = "aābcčdeēfgģhiījkķlļmnņoprsštuūvzžwxyqбвгдёжзийлпуфхцчшщъыьэюяäöüß";
     String[] wordArray;
     List<String> wordList;
     double wordCount;
 
-    public Spelling(String text) throws IOException {
+    public Spelling(String text) {
 
         for(char c : charString.toCharArray()) {
             charList.add(c);
@@ -24,39 +26,42 @@ class Spelling {
         wordList = Arrays.asList(wordArray);
 
         for(String word : wordList){
-            double number = (Double)(Collections.frequency(wordList, word)/wordCount);
+            double number = Collections.frequency(wordList, word)/wordCount;
             dictionary.put(word, number);
         }
     }
 
     private final ArrayList<String> edits(String word) {
-        ArrayList<String> result = new ArrayList<String>();
+        ArrayList<String> result = new ArrayList<>();
         for(int i=0; i < word.length(); ++i)
             result.add(word.substring(0, i) + word.substring(i+1));
         for(int i=0; i < word.length()-1; ++i)
             result.add(word.substring(0, i) + word.substring(i+1, i+2) + word.substring(i, i+1) + word.substring(i+2));
         for(int i=0; i < word.length(); ++i)
             for(char c : charList)
-                result.add(word.substring(0, i) + String.valueOf(c) + word.substring(i+1));
+                result.add(word.substring(0, i) + c + word.substring(i+1));
         for(int i=0; i <= word.length(); ++i)
             for(char c : charList)
-                result.add(word.substring(0, i) + String.valueOf(c) + word.substring(i));
+                result.add(word.substring(0, i) + c + word.substring(i));
         return result;
     }
 
     public final ArrayList<String> correct(String word) {
         word.toLowerCase();
         ArrayList<String> list = edits(word);
-        Map<String, Double> candidates = new HashMap<String, Double>();
-        ArrayList<String> tempList = new ArrayList<String>();
-        Map<Double, String> tempMap = new HashMap<Double, String>();
+        Map<String, Double> candidates = new HashMap<>();
+        ArrayList<String> results = new ArrayList<>();
+        Map<Double, String> tempMap = new HashMap<>();
 
         for(String s : list) {
-            if(dictionary.containsKey(s))
+            if(dictionary.containsKey(s)) {
+                Log.d("CORRECTION_INFO",s);
                 candidates.put(s, dictionary.get(s));
+            }
             for (String w : edits(s))
-                if (dictionary.containsKey(w))
+                if (dictionary.containsKey(w)) {
                     candidates.put(w, dictionary.get(w));
+                }
         }
 
         if(candidates.size() > 0) {
@@ -65,19 +70,19 @@ class Spelling {
             }
             if(candidates.size() > 3){
                 for (int i = 0; i < 3; i++) {
-                    tempList.add(tempMap.get(Collections.max(candidates.values())));
+                    results.add(tempMap.get(Collections.max(candidates.values())));
                     candidates.remove(tempMap.get(Collections.max(candidates.values())));
+                    tempMap.remove(tempMap.get(Collections.max(candidates.values())));
                 }
-                return tempList;
+                return results;
             }
             else{
                 for(String a : candidates.keySet()){
-                    tempList.add(a);
+                    results.add(a);
                 }
-                return tempList;
+                return results;
             }
         }
-
-        return tempList;
+        return results;
     }
 }
